78bd36e33933d6bc2cc526243886f91b1a2b692c
xKosuke <113640887+xKosuke@users.noreply.github.com>
2022-12-14 04:43:39 +0100

    Update rageuiv2.lua

--- rageuiv2.lua
+++ rageuiv2.lua
@@ -7,18 +7,14 @@ Citizen.CreateThread(function()
         end
         RageUI.IsVisible(menu, true, true, true, function()
             RageUI.Button("Option 1", nil, {}, true, function()
-                -- Code à exécuter lorsque l'utilisateur sélectionne l'option 1
             end)
 
             RageUI.Button("Option 2", nil, {}, true, function()
-                -- Code à exécuter lorsque l'utilisateur sélectionne l'option 2
             end)
 
             RageUI.Button("Option 3", nil, {}, true, function()
-                -- Code à exécuter lorsque l'utilisateur sélectionne l'option 3
             end)
         end, function()
-            -- Code à exécuter lorsque l'utilisateur ferme le menu
         end)
         Citizen.Wait(0)
     end
